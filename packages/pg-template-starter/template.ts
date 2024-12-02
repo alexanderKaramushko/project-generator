d071c1edf8f791521afa5c684d2ef3a6f6a69bd0
Aleksandr Karamushko <a.morgoth.b@gmail.com>
2024-12-02 21:47:53 +0400

    описать типы template и указать полные названия конфигов

--- packages/pg-template-starter/template.ts
+++ packages/pg-template-starter/template.ts
@@ -2,7 +2,18 @@ import templateJSON from './template.json';
 
 export type Preset = keyof typeof templateJSON;
 
+type FileStructure = {
+    name: string;
+    content: string;
+    children: FileStructure[];
+}
+
 export type Template = Record<Preset, {
     package: Record<string, any>;
-    configs: Partial<typeof templateJSON[keyof typeof templateJSON]>;
+    configs: {
+        'eslint.config.js'?: Record<string, any>;
+        'jest.config.ts'?: Record<string, any>;
+        'tsconfig.json'?: Record<string, any>;
+    };
+    fileStructure: FileStructure;
 }>
