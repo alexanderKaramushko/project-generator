0f755e6aa0568c405ce836208f9e5d6c28aab7be
Aleksandr Karamushko <a.morgoth.b@gmail.com>
2024-12-08 17:22:29 +0400

    добавить поле сборщика в шаблон

--- packages/pg-template-starter/template.ts
+++ packages/pg-template-starter/template.ts
@@ -5,11 +5,24 @@ export type Preset = keyof typeof templateJSON;
 type FileStructureLibName = string;
 
 export type Template = Record<Preset, {
+    /**
+     * @description package.json
+     */
     package: Record<string, any>;
+    /**
+     * @description Общие конфиги инфраструктуры
+     */
     configs: {
         'eslint.config.js'?: Record<string, any>;
         'jest.config.ts'?: Record<string, any>;
         'tsconfig.json'?: Record<string, any>;
     };
+    /**
+     * @description Файловые структуры по пресету
+     */
     fileStructure: FileStructureLibName;
+    /**
+     * @description Используемый сборщик проекта
+     */
+    builder: string;
 }>
