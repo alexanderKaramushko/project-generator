3d5d56478474e05a82dcd5398ea8c18fb5eeea4c
Aleksandr Karamushko <a.morgoth.b@gmail.com>
2025-02-26 17:34:27 +0400

    перенести запуск билдера в файлы скриптов

--- packages/pg-template-esbuild/scripts/build.js
+++ packages/pg-template-esbuild/scripts/build.js
@@ -1,3 +1,109 @@
-const { execSync } = require('child_process');
+/* eslint-disable no-console */
+/* eslint-disable sort-keys */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/newline-after-import */
+const jsx = require('@svgr/plugin-jsx');
+const svgo = require('@svgr/plugin-svgo');
+const copyStatic = require('esbuild-plugin-copy');
+const dotenv = require('dotenv');
+const { bool, cleanEnv, str } = require('envalid');
+const esbuild = require('esbuild');
+// @ts-expect-error: no @types
+const ignorePlugin = require('esbuild-plugin-ignore');
+const { postcssModules, sassPlugin } = require('esbuild-sass-plugin');
+const path = require('node:path');
+const process = require('node:process');
+// @ts-expect-error: no @types
+const postCssImport = require('postcss-import');
 
-execSync(`cross-env ENV_NAME=.production && tsx ${require.resolve('../esbuild.prod.ts')} && tsc-alias -p tsconfig.alias.json`);
+const { execSync } = require('node:child_process');
+const { envPlugin, svgrPlugin, typeScriptDeclarationsPlugin } = require('./plugins');
+
+execSync('cross-env ENV_NAME=.production');
+
+dotenv.config();
+
+const env = cleanEnv(process.env, {
+  ENTRY: str({
+    default: './src/index.tsx',
+  }),
+  OUTPUT: str({
+    default: './public',
+  }),
+  GENERATE_DECLARATIONS: bool({
+    default: false,
+  }),
+  NODE_ENV: str({
+    default: 'production',
+  }),
+  GLOBAL_SCSS_PATH: str(),
+  SCSS_VARIABLES_PATH: str(),
+  SCSS_MIXINS_PATH: str(),
+  OPEN: bool({ default: false }),
+});
+
+const globalModulePaths = [env.GLOBAL_SCSS_PATH];
+const scssVariablesFolder = path.join(__dirname, env.SCSS_VARIABLES_PATH);
+const scssMixinsFolder = path.join(__dirname, env.SCSS_MIXINS_PATH);
+
+const options = {
+  assetNames: 'assets/[name]',
+  entryPoints: [env.ENTRY, './src/service-worker.ts'],
+  outdir: env.OUTPUT,
+  publicPath: '/',
+  bundle: true,
+  watch: false,
+  minify: true,
+  sourcemap: false,
+  target: 'esnext',
+  format: 'esm',
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+  },
+  plugins: [
+    envPlugin(),
+    sassPlugin({
+      transform: postcssModules(
+        {
+          basedir: '',
+          globalModulePaths,
+        },
+        [postCssImport()],
+      ),
+      precompile(source, pathname, isRoot) {
+        return isRoot
+          ? `
+            @import '${scssVariablesFolder}/index.scss';
+            @import '${scssMixinsFolder}/index.scss';
+            ${source}
+          `
+          : source;
+      },
+    }),
+    ignorePlugin([]),
+    svgrPlugin({
+      dimensions: false,
+      plugins: [svgo, jsx],
+    }),
+    env.GENERATE_DECLARATIONS && typeScriptDeclarationsPlugin(),
+    copyStatic({
+      resolveFrom: 'cwd',
+      assets: {
+        from: ['./src/shared/assets/fonts/**/*'],
+        to: ['./public'],
+      },
+    }),
+  ].filter(Boolean),
+  loader: {
+    '.png': 'file',
+    '.jpeg': 'file',
+    '.jpg': 'file',
+    '.js': 'jsx',
+    '.woff2': 'file',
+    '.svg': 'file',
+  },
+};
+
+esbuild.build(options).catch((err) => {
+  console.error(err);
+});
--- packages/pg-template-esbuild/scripts/start.js
+++ packages/pg-template-esbuild/scripts/start.js
@@ -1,3 +1,127 @@
-const { execSync } = require('child_process');
+/* eslint-disable no-console */
+/* eslint-disable sort-keys */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/newline-after-import */
+const jsx = require('@svgr/plugin-jsx');
+const dotenv = require('dotenv');
+const { bool, cleanEnv, port, str } = require('envalid');
+const esbuild = require('esbuild');
+// @ts-expect-error: no @types
+const ignorePlugin = require('esbuild-plugin-ignore');
+const { postcssModules, sassPlugin } = require('esbuild-sass-plugin');
+const liveServer = require('live-server');
+const path = require('node:path');
+const process = require('node:process');
+// @ts-expect-error: no @types
+const postCssImport = require('postcss-import');
 
-execSync(`cross-env ENV_NAME=.develop ts-node ${require.resolve('../esbuild.dev.ts')}`);
+const { execSync } = require('node:child_process');
+const { envPlugin, svgrPlugin } = require('./plugins');
+
+execSync('cross-env ENV_NAME=.develop');
+
+dotenv.config({
+  path: path.join(__dirname, `../${process.env.ENV_NAME}.env`),
+});
+
+const env = cleanEnv(process.env, {
+  ENTRY: str({
+    default: './src/index.tsx',
+  }),
+  FILE: str({
+    default: 'index.html',
+  }),
+  HOST: str({
+    default: 'localhost',
+  }),
+  OUTPUT: str({
+    default: 'public/index.js',
+  }),
+  PORT: port({
+    default: 3000,
+  }),
+  ROOT: str({
+    default: 'public',
+  }),
+  NODE_ENV: str({
+    default: 'development',
+  }),
+  GLOBAL_SCSS_PATH: str(),
+  SCSS_VARIABLES_PATH: str(),
+  SCSS_MIXINS_PATH: str(),
+  OPEN: bool({ default: false }),
+});
+
+const globalModulePaths = [env.GLOBAL_SCSS_PATH];
+const scssVariablesFolder = path.join(__dirname, env.SCSS_VARIABLES_PATH);
+const scssMixinsFolder = path.join(__dirname, env.SCSS_MIXINS_PATH);
+
+liveServer.start({
+  host: env.HOST,
+  port: env.PORT,
+  file: env.FILE,
+  root: env.ROOT,
+  open: env.open,
+});
+
+const options = {
+  assetNames: 'assets/[name]',
+  entryPoints: [env.ENTRY],
+  outfile: env.OUTPUT,
+  bundle: true,
+  watch: {
+    onRebuild(error) {
+      if (error) {
+        console.error('esbuild: Watch build failed:', error.message);
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('esbuild: Watch build succeeded');
+      }
+    },
+  },
+  minify: false,
+  sourcemap: 'linked',
+  target: 'esnext',
+  format: 'iife',
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+  },
+  plugins: [
+    envPlugin(),
+    sassPlugin({
+      transform: postcssModules(
+        {
+          basedir: '',
+          globalModulePaths,
+        },
+        [postCssImport()],
+      ),
+      precompile(source, pathname, isRoot) {
+        return isRoot
+          ? `
+            @import '${scssVariablesFolder}/index.scss';
+            @import '${scssMixinsFolder}/index.scss';
+            ${source}
+          `
+          : source;
+      },
+    }),
+    ignorePlugin([]),
+    svgrPlugin({
+      dimensions: false,
+      plugins: [jsx],
+    }),
+  ].filter(Boolean),
+  loader: {
+    '.png': 'file',
+    '.jpeg': 'file',
+    '.jpg': 'file',
+    '.js': 'jsx',
+    '.woff2': 'file',
+    '.svg': 'file',
+  },
+};
+
+esbuild.build(options).catch((err) => {
+  console.error(err);
+});
