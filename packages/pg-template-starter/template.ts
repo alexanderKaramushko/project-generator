e787215a0a8a5ea8a905afa0fb5b97515fd4004d
Aleksandr Karamushko <a.morgoth.b@gmail.com>
2025-02-26 20:02:18 +0400

    minor-релиз пакета pg-template-starter

--- packages/pg-template-starter/template.ts
+++ packages/pg-template-starter/template.ts
@@ -8,15 +8,11 @@ export type Template = Record<Preset, {
     /**
      * @description package.json
      */
-    package: Record<string, any>;
+    projects: Record<string, any>;
     /**
      * @description Общие конфиги инфраструктуры
      */
-    configs: {
-        'eslint.config.js'?: Record<string, any>;
-        'jest.config.ts'?: Record<string, any>;
-        'tsconfig.json'?: Record<string, any>;
-    };
+    configs: Record<string, any>;
     /**
      * @description Файловые структуры по пресету
      */
