dc2e0e7ba08cbc71aefcfb890043a70c8af78281
Aleksandr Karamushko <a.morgoth.b@gmail.com>
2025-07-13 18:30:32 +0400

    проверка canary-версии перед release

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
-// 1. prompt, какой пакет нужно публиковать ⏳
-// 2. Проверить, что для текущей релизной версии создавалась canary-версия ⏳
+// 1. prompt, какой пакет нужно публиковать ✅
+// 2. Проверить, что для текущей релизной версии создавалась canary-версия ✅
 // 3. Собрать пакет из переданного commit sha, если есть сборка ⏳
 // 4. Синхронизировать версии ⏳
 // 5. Создать релизную версию ⏳
@@ -10,15 +11,28 @@
 // 8. Опубликовать новый тег ⏳
 // 9. Уведомить в боте и предрелизе ⏳
 
-// const shelljs = require('shelljs');
-// const path = require('node:path');
-// const syncVersion = require('./sync-version');
-// const enlistPackages = require('./enlist-packages');
+const shelljs = require('shelljs');
+const enlistPackages = require('./enlist-packages');
 
 async function run() {
-  // const response = await enlistPackages();
+  const response = await enlistPackages();
 
-  // const packageDirContext = shelljs.cd(response.package.packageDir);
+  try {
+    const versions = JSON.parse(
+      shelljs.cmd('npm', 'view', response.package.packageName, 'versions').stdout.replaceAll('\'', '"'),
+    );
+
+    const lastVersion = versions.at(-1);
+
+    if (!/\d+.\d+.\d+-\d+/.test(lastVersion)) {
+      console.log(`Версия ${lastVersion} пакета ${response.package.packageName} не является предрелизной!`);
+      console.log('Создайте предрелизную версию через run run canary-release.');
+
+      return;
+    }
+  } catch (error) {
+    console.log(`Ошибка парсинга версий: ${error.message}`);
+  }
 }
 
 run();
